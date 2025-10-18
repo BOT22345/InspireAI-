dc915443b77c274967eba3cb0290f3eee4a20eca
BOT22345 <153290500+Nayan2234@users.noreply.github.com>
2025-10-18 17:17:24 +0530

    created all the routes for each page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import {Routes,Route} from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Layout from './pages/Layout.jsx';
+import DashBoard from './pages/Dashboard.jsx';
+import WriteArticle from './pages/WriteArticle.jsx';
+import BlogTitle from './pages/BlogTitles.jsx';
+import GenerateImages from './pages/GenerateImages.jsx';
+import RemoveBackground from './pages/RemoveBackground.jsx';
+import RemoveObject from './pages/RemoveObject.jsx';
+import ReviewResume from './pages/ReviewResume.jsx';
+import Community from './pages/Community.jsx';
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/ai' element={<Layout></Layout>}>
+          <Route index element={<DashBoard></DashBoard>}></Route>
+          <Route path='write-article' element={<WriteArticle></WriteArticle>}></Route>
+          <Route path='blog-titles' element={<BlogTitle></BlogTitle>}></Route>
+          <Route path='generate-images' element={<GenerateImages></GenerateImages>}></Route>
+          <Route path='remove-background' element={<RemoveBackground></RemoveBackground>}></Route>
+          <Route path='remove-object' element={<RemoveObject></RemoveObject>}></Route>
+          <Route path='review-resume' element={<ReviewResume></ReviewResume>}></Route>
+          <Route path='community' element={<Community></Community>}></Route>
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
