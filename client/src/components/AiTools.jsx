0a559636739d4863672eb06cde6513ddd6bdd281
BOT22345 <153290500+Nayan2234@users.noreply.github.com>
2025-10-20 21:16:04 +0530

    created the basic structure of AI tools componenet and integrated it to home page

---
+++ client/src/components/AiTools.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AiTools = () => {
+  return (
+  <>
+    <div className="px-4 sm:px-20 xl:px-32 my-24">
+      <div className="text-center">
+        <h2 className="text-white-700 text-[80px] font-semibold">Powerful <span className="text-primary">AI Tools</span></h2>
+        <p className="text-gray-500 max-w-lg mx-auto text-xl">Everything you need to create, enhance, and optimzie your content with cutting-edge AI technology.</p>
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default AiTools
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
 
 const home = () => {
   return (
     <>
-    <div>
+    <div className="bg-[url('/gradient_background.jpg')] bg-cover bg-no-repeat bg-center min-h-screen text-white">
       <Navbar></Navbar>
       <Hero></Hero>
+      <AiTools></AiTools>
     </div>
     </>
   )
