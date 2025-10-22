59003fb415120ef5567e8cecd137f6e0844d7c6f
BOT22345 <153290500+Nayan2234@users.noreply.github.com>
2025-10-22 11:42:10 +0530

    created the Testimonial component and integrated it into home page

--- client/src/components/AiTools.jsx
+++ client/src/components/AiTools.jsx
@@ -9,7 +9,7 @@
     <>
       <div className="px-4 sm:px-20 xl:px-32 my-24">
         <div className="text-center">
-          <h2 className="text-white-700 text-[80px] font-semibold">Powerful <span className="text-primary">AI Tools</span></h2>
+          <h2 className="text-white-700 text-[60px] font-semibold">Powerful <span className="text-primary">AI Tools</span></h2>
           <p className="text-gray-500 max-w-lg mx-auto text-xl">Everything you need to create, enhance, and optimzie your content with cutting-edge AI technology.</p>
         </div>
         <div className="flex flex-wrap mt-10 justify-center">
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
+import Testimonial from '../components/Testimonial'
 
 const home = () => {
   return (
@@ -10,6 +11,7 @@ const home = () => {
       <Navbar></Navbar>
       <Hero></Hero>
       <AiTools></AiTools>
+      <Testimonial></Testimonial>
     </div>
     </>
   )
