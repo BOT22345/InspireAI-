2b81bef48bac5b90727853d74205240e30476c8b
BOT22345 <153290500+Nayan2234@users.noreply.github.com>
2025-10-23 22:46:55 +0530

    created the dummy payment plan in clerk and integrated it to Plan component

--- client/src/components/Plan.jsx
+++ client/src/components/Plan.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import {PricingTable} from "@clerk/clerk-react"
 const Plan = () => {
   return (
     <div className="max-w-2-xl max-auto z-20 my-30">
         <div className="text-center">
             <h2 className="text-white-700 text-[60px] font-semibold">Choose Your <span className="text-primary"> Plan</span></h2>
             <p className="text-gray-500 max-w-lg mx-auto text-xl">Start for free and scale up as you grow. Find the perfect plan your content creation needs.</p>
+            <div className="mt-14 max-sm:mx-8 w-350 pl-40">
+                <PricingTable></PricingTable>
+            </div>
         </div>
     </div>
   )
